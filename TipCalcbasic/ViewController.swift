6c54f72516db9622baa13d977ffc993573efc059
charnpreetsingh <csingh02@calpoly.edu>
2017-10-08 12:32:13 -0700

    inital commit

--- TipCalcbasic/ViewController.swift
+++ TipCalcbasic/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var actualTipAmount: UILabel!
+    @IBOutlet var totalBillAmount: UILabel!
+    @IBOutlet var initalBillAmount: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let keyboardDismiss = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(keyboardDismiss)
     }
+    
+    @objc func dismissKeyboard() {
+        initalBillAmount.endEditing(true)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func helloWorld(_ sender: Any) {
+        print("Hello world! The app works!")
+    }
+    
+    @IBAction func billAmountEntered(_ sender: Any) {
+        print("bill amount entered")
+    }
 }
 
