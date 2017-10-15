abcb6d1accf0f25be5613cc837707e9f8802b734
Ethan Abrams <ethanabrams@pcp072322pcs.wireless.calpoly.edu>
2017-10-15 12:29:46 -0700

    completed app

--- TipCalcbasic/ViewController.swift
+++ TipCalcbasic/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet var actualTipAmount: UILabel!
     @IBOutlet var totalBillAmount: UILabel!
     @IBOutlet var initalBillAmount: UITextField!
+    @IBOutlet weak var tipIndex: UISegmentedControl!
+    
+    let tipValues = [0.10, 0.15, 0.20]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +35,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func updateTipValue(_ sender: Any) {
+        print("tip percent updated")
+        let bill = Double(initalBillAmount.text!) ?? 0
+        let tip = bill * tipValues[tipIndex.selectedSegmentIndex]
+        actualTipAmount.text = String(format: "$%.2f", tip)
+        totalBillAmount.text = String(format: "$%.2F", tip + bill)
+    }
 
     @IBAction func helloWorld(_ sender: Any) {
         print("Hello world! The app works!")
     }
     
-    @IBAction func billAmountEntered(_ sender: Any) {
+    @IBAction func billAmountEntered(_
+        sender: Any) {
         print("bill amount entered")
+        let bill = Double(initalBillAmount.text!) ?? 0
+        let tip = bill * tipValues[tipIndex.selectedSegmentIndex]
+        actualTipAmount.text = String(format: "$%.2f", tip)
+        totalBillAmount.text = String(format: "$%.2F", tip + bill)
     }
 }
 
